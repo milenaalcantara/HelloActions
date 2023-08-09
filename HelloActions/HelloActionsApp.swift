4a06bbd67aca692e25c77406128fea4ed0a2087a
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2023-08-09 11:22:37 -0300

    Initial Commit

---
+++ HelloActions/HelloActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloActionsApp.swift
+//  HelloActions
+//
+//  Created by Milena Lima de Alcântara on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
